da4e58e404339ae39611df5a48fd1876abea6f4c
nester02 <romaprotcenko1@gmail.com>
2026-09-10 18:50:01 +0000

    port: footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,36 +1,66 @@
-import { NavLink } from 'react-router-dom';
+import { Logo } from '../Logo';
+import { IconChevronUp } from '../Icons';
+import styles from './Footer.module.scss';
+
+const REPO_URL = 'https://github.com/nester02/react_phone-catalog';
 
 export const Footer = () => {
   return (
-    <footer>
-      <NavLink to="/" className="logo">
-        Logo
-      </NavLink>
+    <footer className={styles.footer}>
+      <div className={styles.inner}>
+        <Logo className={styles.logo} />
 
-      <nav>
-        <a
-          href="https://github.com/nester02/react_phone-catalog"
-          target="_blank"
-          rel="noreferrer"
+        <ul className={styles.links}>
+          <li>
+            <a
+              className={styles.link}
+              href={REPO_URL}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+          </li>
+
+          <li>
+            <a
+              className={styles.link}
+              href="https://github.com/nester02"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Contacts
+            </a>
+          </li>
+
+          <li>
+            <a
+              className={styles.link}
+              href={`${REPO_URL}/blob/master/LICENSE`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Rights
+            </a>
+          </li>
+        </ul>
+
+        <button
+          type="button"
+          className={styles.backToTop}
+          onClick={() => {
+            window.scrollTo({
+              top: 0,
+              behavior: 'smooth',
+            });
+          }}
         >
-          Github
-        </a>
-
-        <NavLink to="/contact">Contact</NavLink>
-        <NavLink to="/rights">Rights</NavLink>
-      </nav>
-
-      <button
-        type="button"
-        onClick={() => {
-          window.scrollTo({
-            top: 0,
-            behavior: 'smooth',
-          });
-        }}
-      >
-        Back to top
-      </button>
+          Back to top
+          <span className={styles.backToTopButton}>
+            <IconChevronUp />
+          </span>
+        </button>
+      </div>
     </footer>
   );
 };
